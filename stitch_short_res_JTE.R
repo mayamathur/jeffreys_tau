c370ac87c35c69eada298ce66a77462b628ce180
Maya Mathur <mmathur@stanford.edu>
2024-02-01 05:41:31 -0800

    ...

--- stitch_short_res_JTE.R
+++ stitch_short_res_JTE.R
@@ -1,30 +1,5 @@
 
-# run this interactively in ml load R or via:
-#   sbatch -p qsu,owners,normal /home/groups/manishad/JTE/job_stitch.sbatch
-# sacct --name=job_stitch
-# look at its out file:
-# less /home/groups/manishad/JTE/rmfiles/rm_stitch.out
-# less /home/groups/manishad/JTE/rmfiles/rm_stitch.err
-# shift-g to jump to bottom of files
-
-# for non-huge simulations, can often run this script interactively in a higher-memory
-#  Sherlock session:
-# ml load R/4.1.2
-# srun --mem=32G --time=3:00:00 --pty bash
-# R
-
-
-# to be run by stitch.sbatch or manually
-# To quickly run this script in high-mem interactive session:
-# setwd("/home/groups/manishad/JTE"); source("stitch_on_sherlock_JTE.R")
-
-# # load command line arguments
-# args = commandArgs(trailingOnly = TRUE)
-# start.num = as.numeric( args[1] )  # starting results number to stitch
-# stop.num = as.numeric( args[2] )  # stopping results number to stitch
-
-
-
+# can be run in interactive session - :)!
 
 # PRELIMINARIES ----------------------------------------------
 
@@ -44,7 +19,6 @@ source("analyze_sims_helper_JTE.R")
 
 # MAKE AGG DATA - FROM PRE-AGGREGATED SHORT RESULTS ----------------------------------------------
 
-# can be run in interactive session - :)!
 .results.singles.path = "/home/groups/manishad/JTE/short_results"
 .results.stitched.write.path = "/home/groups/manishad/JTE/overall_stitched"
 .name.prefix = "short_results"
