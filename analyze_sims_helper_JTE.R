1642de1b4d2af5e27a2b92b1a3b7137b45f2733f
Maya Mathur <mmathur@stanford.edu>
2024-02-10 06:57:21 -0500

    tweak performance metrics

--- analyze_sims_helper_JTE.R
+++ analyze_sims_helper_JTE.R
@@ -50,6 +50,9 @@ make_agg_data = function( .s,
     "MhatCover",
     "ShatCover",
     
+    "MhatCoverNominal",
+    "ShatCoverNominal",
+    
     "MhatWidth",
     "ShatWidth",
     
@@ -297,8 +300,8 @@ make_agg_data = function( .s,
   agg = s3[ !duplicated(s3$unique.scen), ]
   
   ##### Create Variables That Are Defined At Scenario Rather Than Iterate Level #####
-  agg = agg %>% mutate( GoodMhatCover = MhatCover > goodCoverageCutoff,
-                        GoodShatCover = ShatCover > goodCoverageCutoff )
+  agg = agg %>% mutate( MhatCoverNominal = MhatCover > goodCoverageCutoff,
+                        ShatCoverNominal = ShatCover > goodCoverageCutoff )
   
   return(agg %>% ungroup() )
 }
@@ -1471,7 +1474,7 @@ init_var_names = function(.agg) {
   #  upon reading in data
   estNames <<- c("Mhat", "Shat")
   
-  mainYNames <<- c("Bias", "MAE", "RMSE", "Cover", "Width", "EmpSE")
+  mainYNames <<- c("Bias", "MAE", "RMSE", "Cover", "GoodCover", "Width", "EmpSE")
   
   otherYNames <<- c("EstFail", "CIFail", "RhatGt1.01", "RhatGt1.05")
   
@@ -1786,76 +1789,6 @@ quick_ci = function( est, var ) {
 quick_pval = function( est, var ) {
   2 * ( 1 - pnorm( abs( est / sqrt(var) ) ) )
 }
-
-
-# # STRAIGHT FROM MRM:
-# # summarize performance metrics given a dataset (dat) that is already scenario-aggregated
-# #  looks for all variables with "Bias" or "Cover" in their names and takes their means
-# # description: description of the row to make a nice table
-# # selectVars: "Phat", "Diff" (by default looks for a global variable by this name)
-# my_summarise = function(dat,
-#                         description = NA,
-#                         .selectVars = selectVars,
-#                         badCoverageCutoff = 0.85,
-#                         badWidthCutoff = 0.90,
-#                         averagefn = "mean"
-# ){
-#   
-#   # variables whose average should be taken
-#   meanVars = c( namesWith(pattern = "Bias", dat = dat), 
-#                 namesWith(pattern = "AbsErr", dat = dat),
-#                 namesWith(pattern = "Cover", dat = dat),
-#                 namesWith(pattern = "Width", dat = dat) )
-#   
-#   if (.selectVars == "Phat") meanVars = meanVars[ !grepl(pattern = "Diff", x = meanVars) ]
-#   
-#   if (.selectVars == "Diff") meanVars = meanVars[ !grepl(pattern = "Phat", x = meanVars) ]
-#   
-#   
-#   if (averagefn == "mean") avgfun = function(x) meanNA(x)
-#   if (averagefn == "median") avgfun = function(x) medNA(x)
-#   if (averagefn == "median.pctiles") avgfun = function(x) medNA_pctiles(x)
-#   
-#   # make a one-row summary
-#   tab = dat %>% 
-#     summarise_at( .vars = meanVars, 
-#                   function(x) avgfun(x) )
-#   
-#   # proportion of SCENARIOS with bad MEAN coverage
-#   tab = tab %>% add_column(BadPhatCover = mean(dat$CoverPhat < badCoverageCutoff),
-#                            BadDiffCover = mean(dat$CoverDiff < badCoverageCutoff))
-#   if (.selectVars == "Phat") tab = tab %>% select(-BadDiffCover)
-#   if (.selectVars == "Diff") tab = tab %>% select(-BadPhatCover)
-#   
-#   # proportion of SCENARIOS with bad median CI width
-#   tab = tab %>% add_column(BadPhatWidth = mean(dat$PhatCIWidth > badWidthCutoff),
-#                            BadDiffWidth = mean(dat$DiffCIWidth > badWidthCutoff) )
-#   if (.selectVars == "Phat") tab = tab %>% select(-BadDiffWidth)
-#   if (.selectVars == "Diff") tab = tab %>% select(-BadPhatWidth)
-#   
-#   # only round selected columns if we have strings
-#   #  (e.g., median with percentiles)
-#   if ( averagefn == "median.pctiles" ){
-#     
-#     if (.selectVars == "Phat"){
-#       tab$BadPhatCover = round(tab$BadPhatCover, 2)
-#       tab$BadPhatWidth = round(tab$BadPhatWidth, 2)
-#     }
-#     
-#     if (.selectVars == "Diff"){
-#       tab$BadDiffCover = round(tab$BadDiffCover, 2)
-#       tab$BadDiffWidth = round(tab$BadDiffWidth, 2)
-#     } 
-#     
-#   } else {  # otherwise round all columns
-#     tab = round( tab, 2 )
-#   }
-#   
-#   tab = tab %>% add_column(n.scens = nrow(dat), .before = 1 )
-#   
-#   if ( !is.na(description) ) tab = tab %>% add_column(Scenarios = description, .before = 1)
-#   return(tab)
-# }
 
 
 
